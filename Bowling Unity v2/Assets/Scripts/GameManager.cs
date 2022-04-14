58a287ad1650a7a61d3e694867d97e3851a1f5cb
ashley-m0 <ashley.mojica@knights.ucf.edu>
2022-04-14 19:01:24 -0400

    Reset Ball and Pins Update
    Resets the ball and pins once it reaches the ball pit.

--- Bowling Unity v2/Assets/Scripts/GameManager.cs
+++ Bowling Unity v2/Assets/Scripts/GameManager.cs
@@ -9,13 +9,23 @@ public class GameManager : MonoBehaviour
     public GameObject ball;
     public GameObject[] pins;
     public int score = 0;
+    public int turnsCounter = 0; 
     public Text pointsUI;
+
+    private Vector3[] positions;
+    private Vector3 ballPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-
         pins = GameObject.FindGameObjectsWithTag("Pin");
+        positions = new Vector3[pins.Length];
+        ballPosition = ball.transform.position;
+
+        for(int i = 0; i < pins.Length; i++)
+        {
+            positions[i] = pins[i].transform.position;
+        }
     }
 
     // Update is called once per frame
@@ -23,7 +33,9 @@ public class GameManager : MonoBehaviour
     {
         if (ball.transform.position.y < -1)
         {
-            CountPinsDown(); 
+            CountPinsDown();
+            turnsCounter++;
+            ResetPins();
         }
     }
 
@@ -38,5 +50,16 @@ public class GameManager : MonoBehaviour
             }
         }
         pointsUI.text = score.ToString();
+    }
+
+    void ResetPins()
+    {
+        for(int i = 0; i < pins.Length; i++)
+        {
+            pins[i].SetActive(true);
+            pins[i].transform.position = positions[i];
+        }
+
+        ball.transform.position = ballPosition;
     }
 }
