ac63d1f79747a566ee90292cd4164e763ba2bfe0
ashley-m0 <ashley.mojica@knights.ucf.edu>
2022-04-14 18:48:04 -0400

    Points Update
    Implements point system when pins are knocked down.

--- Bowling Unity v2/Assets/Scripts/GameManager.cs
+++ Bowling Unity v2/Assets/Scripts/GameManager.cs
@@ -1,37 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    //Score and Turn Management
-
-    private GameObject ball;
-
+    public GameObject ball;
+    public GameObject[] pins;
     public int score = 0;
-    GameObject[] pins;
+    public Text pointsUI;
 
     // Start is called before the first frame update
     void Start()
     {
-        pins = GameObject.FindGameObjectsWithTag("Pins");
+
+        pins = GameObject.FindGameObjectsWithTag("Pin");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(ball.transform.position.y < -0.8){
-            CountPinsDown();
+        if (ball.transform.position.y < -1)
+        {
+            CountPinsDown(); 
         }
     }
 
-    void CountPinsDown(){
-        for(int i = 0; i < pins.Length; i++){
-            if (pins[i].transform.eulerAngles.z > 5 && pins[i].transform.eulerAngles.z > 355 ){
+    void CountPinsDown()
+    {
+        for(int i = 0; i < pins.Length; i++)
+        {
+            if (pins[i].transform.eulerAngles.z > 5 && pins[i].transform.eulerAngles.z < 355 && pins[i].activeSelf)
+            {
                 score++;
-                //pins[i].SetActive(false);
+                pins[i].SetActive(false);
             }
         }
-        Debug.Log(score);
+        pointsUI.text = score.ToString();
     }
 }
