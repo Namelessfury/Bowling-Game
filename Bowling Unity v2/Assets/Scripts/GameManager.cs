df88edeb1cf39c8de9fe800b186636ecf1264053
ashley-m0 <ashley.mojica@knights.ucf.edu>
2022-04-17 18:58:38 -0400

    Background and Level update
    Added walls to the lane and started adding in the objects for the different levels

--- Bowling Unity v2/Assets/Scripts/GameManager.cs
+++ Bowling Unity v2/Assets/Scripts/GameManager.cs
@@ -8,6 +8,10 @@ public class GameManager : MonoBehaviour
 {
     public GameObject ball;
     public GameObject[] pins;
+    public GameObject[] books; //level 1 objects
+    public GameObject[] cans; //level 2 objects
+
+    public int level = 0; 
     public int score = 0;
     public int turnsCounter = 1; 
     public Text pointsUI;
@@ -22,6 +26,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Get the pins positions 
         pins = GameObject.FindGameObjectsWithTag("Pin");
         pinPositions = new Vector3[pins.Length];
         pinRotation = pins[0].transform.rotation;
@@ -32,15 +37,21 @@ public class GameManager : MonoBehaviour
         {
             pinPositions[i] = pins[i].transform.position;
         }
+
+        //Get Level Selected
+        SelectLevel(level);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         //Once the user has two turns
         if (turnsCounter == 2)
         {
             ResetPins();
+            ResetCans();
         }
 
         //Once the ball lands in the ball pit
@@ -93,6 +104,64 @@ public class GameManager : MonoBehaviour
         ball.transform.position = ballPosition;
         ball.transform.rotation = ballRotation;
         OnPinsReset?.Invoke(this, EventArgs.Empty);
+    }
+
+    void SelectLevel(int level)
+    {
+        switch (level)
+        {
+            case 1:
+                //books = GameObject.FindGameObjectsWithTag("Book");
+
+                break;
+            case 2:
+                cans = GameObject.FindGameObjectsWithTag("Can");
+                setCans();
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void setCans()
+    {
+        setObjectsStanding(cans);
+
+        cans[0].transform.position = new Vector3(2.36f, -0.299f, -65.43811f);
+        cans[1].transform.position = new Vector3(1.09f, -0.299f, -58.18f);
+        cans[2].transform.position = new Vector3(-1.99f, -0.299f, -61.1f);
+        cans[3].transform.position = new Vector3(-3.44f, -0.299f, -67.89f);
+        cans[4].transform.position = new Vector3(3.73f, -0.299f, -60.94f);
+
+    }
+
+    void ResetCans()
+    {
+        setObjectsStanding(cans);
+
+        cans[0].transform.position = new Vector3(7.909999f, 30.07f, -63.6f);
+        cans[1].transform.position = new Vector3(8.46f, 30.07f, -64.48f);
+        cans[2].transform.position = new Vector3(7.931f, 30.07f, -65.59f);
+        cans[3].transform.position = new Vector3(8.759998f, 30.07f, -66.24f);
+        cans[4].transform.position = new Vector3(7.126f, 30.07f, -64.882f);
+    }
+
+    void setObjectsStanding(GameObject[] obj)
+    {
+        for (int i = 0; i < obj.Length; i++)
+        {
+            Rigidbody RB = obj[i].GetComponent<Rigidbody>();
+            obj[i].SetActive(true);
+            RB.velocity = Vector3.zero;
+            RB.angularVelocity = Vector3.zero;
+            obj[i].transform.rotation = pinRotation;
+        }
     }
 }
 
