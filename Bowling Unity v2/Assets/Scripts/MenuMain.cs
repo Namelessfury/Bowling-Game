5127c00dc21ce9136f779c793f39a1e372cec96f
Namelessfury <69160890+Namelessfury@users.noreply.github.com>
2022-04-22 17:26:42 -0400

    Updated Shop
    The Shop now works

--- Bowling Unity v2/Assets/Scripts/MenuMain.cs
+++ Bowling Unity v2/Assets/Scripts/MenuMain.cs
@@ -1,3 +1,10 @@
+/*  File Name:         MainMenu.cs  
+ *  Purpose:           Functionality of the main menu buttons. 
+ *  Contributors:      Jose Valen
+ *                     Myles Caesar
+ *  Last Modified:     4/22/2022 - Myles Caesar
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +13,11 @@ using UnityEngine.SceneManagement;
 
 public class MenuMain : MonoBehaviour
 {
-    public UnityEngine.UI.Text CounterText;
+    public Text CounterText; //Points display on the main menu
 
     void Start()
     {
+        //Gets player's saved data and updates points display
         PlayerData data = SaveSystem.LoadPlayer();
         int PlayerPoints = data.points;
         updatePointCounter(PlayerPoints);
@@ -17,21 +25,25 @@ public class MenuMain : MonoBehaviour
 
     public void PlayGame()
     {
+        //Goes to level select
         SceneManager.LoadScene("Level Select");
     }
 
     public void GoToShop() 
     {
+        //Goes to the game shop
         SceneManager.LoadScene("GameShop");
     }
 
     public void ExitGame()
     {
+        //Quits the game
         Application.Quit();
     }
 
     void updatePointCounter(int Display) 
     {
-        CounterText.GetComponent<Text>().text = Display.ToString(); 
+        //Updates the points on the main menu
+        CounterText.text = Display.ToString(); 
     }
 }
--- Bowling Unity v2/Assets/Scripts/ShopMain.cs
+++ Bowling Unity v2/Assets/Scripts/ShopMain.cs
@@ -1,3 +1,10 @@
+/*  File Name:         ShopMain.cs  
+ *  Purpose:           Manages the shop logic. 
+ *  Contributors:      Jose Valen
+ *                     Myles Caesar
+ *  Last Modified:     4/22/2022 - Myles Caesar
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,80 +12,67 @@ using UnityEngine.SceneManagement;
 
 public class ShopMain : MonoBehaviour
 {
+    //Buy the Lucky Ball
     public void BuyLuckyBall() {
 
-        PlayerData Data = SaveSystem.LoadPlayer();
-        int CurrentPoints = Data.points;
-        int[] localInventory = Data.inventory;
-
-        if (CurrentPoints < 30)
-            return;
-
-        if (localInventory[1] == 0)
-            localInventory[1] = 1;
-        if (localInventory[1] == 1) {
-            localInventory[1] = 2;
-            if (localInventory[0] == 2)
-                localInventory[0] = 1;
-            if (localInventory[2] == 2)
-                localInventory[2] = 1;
-            if (localInventory[3] == 2)
-                localInventory[3] = 1;
-        }
-
-        SaveSystem.SavePlayer(CurrentPoints - 30, localInventory);
+        BuyBall(30, 1);
     }
 
+    //Buy the Sky Ball
     public void BuySkyBall() {
 
-        PlayerData Data = SaveSystem.LoadPlayer();
-        int CurrentPoints = Data.points;
-        int[] localInventory = Data.inventory;
-
-        if (CurrentPoints < 50)
-            return;
-
-        if (localInventory[2] == 0)
-            localInventory[2] = 1;
-        if (localInventory[2] == 1)
-        {
-            localInventory[2] = 2;
-            if (localInventory[0] == 2)
-                localInventory[0] = 1;
-            if (localInventory[1] == 2)
-                localInventory[1] = 1;
-            if (localInventory[3] == 2)
-                localInventory[3] = 1;
-        }
-
-        SaveSystem.SavePlayer(CurrentPoints - 50, localInventory);
+        BuyBall(50, 2);
     }
 
+    //Buy the Cosmic Ball
     public void BuyCosmicBall() {
 
-        PlayerData Data = SaveSystem.LoadPlayer();
-        int CurrentPoints = Data.points;
-        int[] localInventory = Data.inventory;
-
-        if (CurrentPoints < 70)
-            return;
-
-        if (localInventory[3] == 0)
-            localInventory[3] = 1;
-        if (localInventory[3] == 1)
-        {
-            localInventory[3] = 2;
-            if (localInventory[0] == 2)
-                localInventory[0] = 1;
-            if (localInventory[1] == 2)
-                localInventory[1] = 1;
-            if (localInventory[2] == 2)
-                localInventory[2] = 1;
-        }
-
-        SaveSystem.SavePlayer(CurrentPoints - 70, localInventory);
+        BuyBall(70, 3);
     }
 
+    //Buy a certain ball
+    void BuyBall(int cost, int index)
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+        int currentPoints = data.points;
+        int[] localInventory = data.inventory;
+
+        switch (localInventory[index]) 
+        {
+            case 2: //Already have the ball equipped
+                return;
+            case 1: //Already own the ball but it isn't equipped
+                localInventory = EquipBall(index, localInventory);  //Equip ball
+                SaveSystem.SavePlayer(currentPoints, localInventory);   //Save points and inventory
+                break;
+            case 0: //Don't own the ball
+                if (currentPoints < cost)   //Not enough points to buy the ball
+                    return;
+
+                localInventory = EquipBall(index, localInventory);  //Equip ball
+                SaveSystem.SavePlayer(currentPoints - cost, localInventory);    //Save remaining points and inventory
+                break;
+            default:
+                break;
+        }
+    }
+
+    //Equip a certain ball
+    int[] EquipBall(int index, int[] localInventory)
+    {
+        for (int i = 0; i < localInventory.Length; i++)
+        {
+            if (localInventory[i] == 2) //the last ball that was equipped
+                localInventory[i] = 1;
+
+            if (i == index)             //the ball we are trying to equip
+                localInventory[i] = 2;
+        }
+
+        return localInventory;
+    }
+
+    //Returns to the main menu
     public void BackToMenu() 
     {
         SceneManager.LoadScene("MainMenu");
